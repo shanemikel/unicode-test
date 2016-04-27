7fa8eee0e3c84c5df6a10269d502c66f7bbbfc4a
Shane Pearlman <shanemikel1@gmail.com>
2016-04-27 16:24:32 -0700

    test 3

--- gadts.hs
+++ gadts.hs
@@ -12,3 +12,16 @@ test ∷ IO ()
 test = do
   x ← return "hello"
   putStrLn x
+
+-- id' = λ x → x
+
+data V a = V a a a deriving (Show, Eq)
+
+(·) ∷ Num a ⇒ V a → V a → a
+(V aî bĵ ck) · (V dî eĵ fk) = aî * dî + bĵ * eĵ + ck * fk
+
+(×) ∷ Num a ⇒ V a → V a → V a
+(V aî bĵ ck) × (V dî eĵ fk) = V xî (-yĵ) zk
+  where xî = bĵ * fk - ck * eĵ
+        yĵ = aî * fk - ck * dî
+        zk = aî * eĵ - bĵ * dî
